cd12b9ff928cf4c2b978ad137f6a71e9a009bc5c
Lucas Bastidas <lucaasbastidas@hotmail.com>
2022-11-25 11:25:26 -0300

    add telphone number

--- pages/api/me/index.ts
+++ pages/api/me/index.ts
@@ -12,9 +12,9 @@ const handler = methods({
 		if (!req.body) {
 			return res.status(204).send({ message: "No data for update" });
 		}
-		const { address, name, email } = req.body;
-		if (!address && !name && !email) {
-			return res.status(204).json({ message: "No data for update" });
+		const { address, name, email, tel } = req.body;
+		if (!address && !name && !email && !tel) {
+			return res.status(204).send({ message: "No data for update" });
 		}
 		const updateData = await updateUserData(token.userId, req.body);
 		return res.send({ updateData });
